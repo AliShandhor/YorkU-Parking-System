703a5bb87c6ca8431e4c14cde024e57313e97c69
AliShandhor <a.shanzoor@gmail.com>
2025-03-12 00:37:25 -0400

    Implementing Factory Method; creating UserFactory to create User Types objects.

---
+++ src/main/java/com/yorku/parkingsystem/user/UserFactory.java
@@ -0,0 +1,19 @@
+package com.yorku.parkingsystem.user;
+
+public class UserFactory {
+
+    // Factory method to create different types of users
+    public static User getUser(String clientType, int userID, String firstName, String lastName, String licensePlate) {
+        // Check the clientType and return the corresponding user subclass
+        if (clientType.equalsIgnoreCase("STUDENT")) {
+            return new Student(userID, firstName, lastName, clientType, licensePlate);
+        } else if (clientType.equalsIgnoreCase("FACULTY")) {
+            return new FacultyMember(userID, firstName, lastName, clientType, licensePlate);
+        } else if (clientType.equalsIgnoreCase("VISITOR")) {
+            return new Visitor(userID, firstName, lastName, clientType, licensePlate);
+        } else {
+            // If the clientType is invalid, throw an exception or return null
+            throw new IllegalArgumentException("Invalid client type: " + clientType);
+        }
+    }
+}
