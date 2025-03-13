9d17be230b1c7d4e5e4d5267411a1d61bc53d967
Uzma Alam <uzmaalam@uzmas-mbp.home>
2025-03-13 04:44:52 -0400

    add interface for UserViistor

--- src/main/java/com/yorku/parkingsystem/user/UserVisitor.java
+++ src/main/java/com/yorku/parkingsystem/user/UserVisitor.java
@@ -1,1 +1,14 @@
+package com.yorku.parkingsystem.user;
+/*
+ * interface for the visitor pattern to accept the visitor
+ */
+public interface UserVisitor {
+	void visit(NonFacultyMember nonfacultymember);
+	void visit(FacultyMember facultymember);
+	void visit(Visitor visitor);
+	void visit(Student student);
+	
+	
+
+}
 
