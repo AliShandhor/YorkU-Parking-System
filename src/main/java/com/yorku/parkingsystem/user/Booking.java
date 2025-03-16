4450cf848d802dd8e629ac2b11e570785501ef72
uzma <aintgotnoway12@gmail.com>
2025-03-16 00:56:25 -0400

    update booking.java to integrate with parking

--- src/main/java/com/yorku/parkingsystem/user/Booking.java
+++ src/main/java/com/yorku/parkingsystem/user/Booking.java
@@ -3,72 +3,64 @@ package com.yorku.parkingsystem.user;
 import java.util.Date;
 
 public class Booking {
-	private String bookingID;
-	private ParkingSpace parkingSpace;
+	private int parkingSpotID;
+	private int bookingID;
 	private User user;
-	private Date startTime;
-	private Date endTime;
-	private Payment payment;
-	private boolean no_show; 
+	private Date bookingTime;
+	private int duration;
 	
-	public Booking(String bookingID, ParkingSpace parkingSpace, User user, Date startTime, Date endTime, Payment payment) {
+	public Booking(int parkingSpotID, int bookingID, User user,  Date bookingTime, int duration) {
+		this.parkingSpotID = parkingSpotID;
 		this.bookingID = bookingID;
-        this.parkingSpace = parkingSpace;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.payment = payment;
-        this.user = user;
-        this.no_show = false;
-    
+		this.user = user;
+		this.bookingTime = bookingTime;
+		this.duration = duration;
 	}
 	
 	/*
 	 * confirm and cancel booking
-	 * not refunded if no show
+	 * 
 	 */
 	
 	public void confirm() {
 		BillingVisitor billingVisitor = new BillingVisitor();
 		user.accept(billingVisitor);
 		
-		System.out.println("Booking confirmed for: " + user.getName() + bookingID);
+		System.out.println("Booking confirmed for: " + user.getName() + " Booking ID: " + bookingID);
 	}
-	/*
+	
 	public void cancel() {
-		if (!no_show) {
-			 payment.refundPayment(payment.getAmount());
-		} else {
-			System.out.println("No refund for no-show.");
-		}
+		BillingVisitor billingVisitor = new BillingVisitor();
+		user.accept(billingVisitor);
+		
+		System.out.println("Booking cancelled for: " + user.getName() + " Booking ID: " + bookingID);
 	}
-	*/
-	public void setNo_show(boolean no_show) {
-		this.no_show = no_show;
+	
+	public int getParkingSpotID() {
+		return parkingSpotID;
 	}
-
-	public String getBookingID() {
+	
+	public int getBookingID() {
 		return bookingID;
 	}
-
-	public ParkingSpace getParkingSpace() {
-		return parkingSpace;
-	}
-
+	
 	public User getUser() {
 		return user;
 	}
-
-	public Date getStartTime() {
-		return startTime;
+	
+	public Date getBookingTime() {
+		return bookingTime;
 	}
-
-	public Date getEndTime() {
-		return endTime;
+	
+	public int getDuration() {
+		return duration;
 	}
-
-	public Payment getPayment() {
-		return payment;
-	}
+	
+	
+	
+	
+	
+	
 
 
 	
