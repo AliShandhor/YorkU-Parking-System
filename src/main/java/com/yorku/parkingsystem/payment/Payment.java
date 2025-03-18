46faef7ec1b7e9c453f4dba8161f9e0f295f73a9
uzma <aintgotnoway12@gmail.com>
2025-03-18 02:31:38 -0400

    add context class payment to refer to paymentstrategy for executing designated method

--- src/main/java/com/yorku/parkingsystem/payment/Payment.java
+++ src/main/java/com/yorku/parkingsystem/payment/Payment.java
@@ -1,5 +1,21 @@
 package com.yorku.parkingsystem.payment;
-
+//context class
 public class Payment {
+    private PaymentStrategy paymentStrategy;
 
+    public Payment(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void checkout(double amount) {
+        if (paymentStrategy != null) {
+            paymentStrategy.pay(amount); //selected strategy
+        } else {
+            System.out.println("No payment method is selected.");
+        }
+    }
 }
